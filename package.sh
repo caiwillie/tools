2820c1c7ba15abc23ceb0ca7097742d5988d347b
caiwillie <caiwl@shinemo.com>
2022-03-27 21:45:59 +0800

    更新代码

--- package.sh
+++ package.sh
@@ -6,8 +6,12 @@ BASE_HOME=$(dirname $SCRIPT_ABSPATH)
 
 app=$1
 
+maven_setting=${BASE_HOME}/.maven/aliyun.xml
+
+cmd="mvn -s ${maven_setting} -Dmaven.test.skip=true -U"
+
 if [ "${app}" == "all" ]; then
-  mvn -Dmaven.test.skip=true -U clean package
+  cmd="${cmd} clean package"
   exit 0
 elif [ -z "${app}" ]; then
   echo "应用名称不能为空"
@@ -15,12 +19,10 @@ elif [ -z "${app}" ]; then
 elif [ ! -d "${BASE_HOME}/${app}" ]; then
   echo "应用 ${app} 不存在"
   exit 1
+else
+  cmd="${cmd} -am -pl com.caiwillie.tools:${app} clean package"
 fi
 
 echo -e "正在打包: ${app}"
-
-cmd="mvn"
-
-cmd="${cmd} -Dmaven.test.skip=true -U -am -pl com.caiwillie.tools:${app} clean package"
 
 ${cmd}
